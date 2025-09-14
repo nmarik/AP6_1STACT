972086a6ae1f8b36e58dec6f8355b93eec2c2632
Marik <ninsanin123@gmail.com>
2025-09-14 19:01:45 +0800

    new file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          HELLO, THIS IS NINA MARIE "MARIK" C. AURESTILA
+        </p>
+        <a
+          className="Contact Me"
+          href="https://www.messenger.com/e2ee/t/1301615228294570"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Me Here!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
